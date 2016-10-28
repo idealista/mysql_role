b38e473bcfc1b403eae0d50944bcf500d7ab3d3b
Alberto Garcia Fernandez <agarcia@idealista.com>
2016-10-28 15:17:12 +0200

    DEVOPS-144 Latest sonarqube versions work on MySQL 5.6 and 5.7 only. Add testsapt

---
+++ tests/mysql.py
@@ -0,0 +1,32 @@
+import pytest
+
+
+@pytest.fixture()
+def AnsibleDefaults(Ansible):
+    return Ansible("include_vars", "defaults/main.yml")["ansible_facts"]
+
+
+def test_mysql_packages(Package):
+    assert Package("mysql-apt-config").is_installed
+    assert Package("python-mysqldb").is_installed
+    assert Package("mysql-common").is_installed
+    assert Package("mysql-server").is_installed
+
+
+def test_mysql_service(File, Service, Socket):
+    assert File("/etc/init.d/mysql")
+    assert Service("mysql").is_enabled
+    assert Service("mysql").is_running
+    assert Socket("tcp://127.0.0.1:3306")
+
+
+def test_mysql_conf(File, AnsibleDefaults):
+    conf = File(AnsibleDefaults["mysql_config_file"])
+    assert conf.contains('Ansible managed config')
+
+
+def test_mysql_database(Command, AnsibleDefaults):
+    BD = "echo \'show databases;\'|mysql -u root -pvagrant|grep -w test01"
+    User = "echo \'show databases;\'|mysql -u testing -ptesting|grep -w testing"
+    assert Command(BD).rc is 0
+    assert Command(User).rc is 0
